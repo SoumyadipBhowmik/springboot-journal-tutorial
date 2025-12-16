8ba8c991b611d3658d96f50e9631d8945e9c5592
Soumyadip <bhowmik.soumyadip9@gmail.com>
2025-12-17 01:53:20 +0530

    removed public modifier from the interface members as good practice

--- src/main/java/net/engineeringdigest/journalApp/service/JournalService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/JournalService.java
@@ -7,16 +7,16 @@ import java.util.List;
 import java.util.Optional;
 
 public interface JournalService {
-    public JournalEntry createJournalEntry(JournalEntry entry);
+    JournalEntry createJournalEntry(JournalEntry entry);
 
-    public JournalEntry createJournalEntry(JournalEntry entry, String userName);
+    JournalEntry createJournalEntry(JournalEntry entry, String userName);
 
-    public Optional<JournalEntry> getJournalEntryById(ObjectId id);
+    Optional<JournalEntry> getJournalEntryById(ObjectId id);
 
-    public List<JournalEntry> findJournalEntriesByUsername(String userName);
+    List<JournalEntry> findJournalEntriesByUsername(String userName);
 
-    public boolean deleteJournalEntryById(ObjectId id, String userName);
+    boolean deleteJournalEntryById(ObjectId id, String userName);
 
-    public JournalEntry updateJournalEntry(ObjectId id, JournalEntry entry);
+    JournalEntry updateJournalEntry(ObjectId id, JournalEntry entry);
 
 }
--- src/main/java/net/engineeringdigest/journalApp/service/UserService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/UserService.java
@@ -7,21 +7,21 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
-    public void createUser(User user);
+    void createUser(User user);
 
-    public void saveUser(User user);
+    void saveUser(User user);
 
-    public void createAdminUser(User user);
+    void createAdminUser(User user);
 
-    public List<User> getAllUsers();
+    List<User> getAllUsers();
 
-    public Optional<User> getUserById(ObjectId id);
+    Optional<User> getUserById(ObjectId id);
 
-    public User updateUserByUserName(String userName, User user);
+    User updateUserByUserName(String userName, User user);
 
-    public void deleteUserById(ObjectId id);
+    void deleteUserById(ObjectId id);
 
-    public void deleteByUsername(String userName);
+    void deleteByUsername(String userName);
 
-    public User findByUserName(String userName);
+    User findByUserName(String userName);
 }
